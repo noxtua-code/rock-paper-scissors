4c5efa772d056e7cb3e08034684f69ed147b25a3
noxtua-code <[200~93567294+noxtua-code@users.noreply.github.com>
2022-12-28 12:02:33 +1000

    Made initital adjustments to js file so the game runs when the player clicks on the button and logs the round result on the screen rather than the console log.

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const gameSelections = ['rock', 'paper', 'scissors'];
+const playerSelectionButtons = document.querySelectorAll('button');
+const roundResultsText = document.querySelector('.round-results')
+let numOfRounds = 5;
+let playerCount = 0;
+let computerCount = 0;
+
+playerSelectionButtons.forEach(element => {
+    element.addEventListener('click', () => {
+        let playerSelection = element.id
+        let computerSelection = getComputerChoice();
+        let result = playRound(computerSelection, playerSelection);
+    });                   
+});
 
 function capitaliseFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-function getNumberOfRounds() {
-    let numOfRounds; 
-
-    while (true) {
-        numOfRounds = +prompt('How many rounds would you like to play?');
-        if (numOfRounds !== 0 && !(isNaN(numOfRounds)))
-            break;
-        console.log('Please enter a number')
-    }
-
-    return numOfRounds;
 }
 
 function getComputerChoice() {  // randomly returns rock/paper/scissors
@@ -22,38 +22,34 @@ function getComputerChoice() {  // randomly returns rock/paper/scissors
     return gameSelections[randomNumber];  
 }  
 
-function getPlayerChoice() { // prompts player for selection, and only accepts input if it matches gameSelections
-    let gettingPrompt = true;
-    let selection; 
-    
-    while (gettingPrompt) {
-        selection = prompt('Select a choice between rock, papers or scissors:').toLowerCase();
-        if (gameSelections.includes(selection)){
-            gettingPrompt = false;
-        } else {
-            console.log('Please type selection again')
-        }
-    }
-    return selection;
-}    
+// function getPlayerChoice() { // gets player choice from clicked button
+//         let selection;
+
+//         playerSelectionButtons = document.querySelectorAll('button');
+//         playerSelectionButtons.forEach(element => {
+//             element.addEventListener('click', () => selection = element.id);
+//         });
+
+//         return selection;
+// }    
 
 function playRound(computerSelection, playerSelection) { //determines the result of the round 
     let result;
     
     switch(true) {
         case playerSelection === computerSelection:
-            console.log(`You tie! You both selected ${playerSelection}`)
-            result = 'tie'
+            roundResultsText.textContent = `You tie! You both selected ${playerSelection}`;
+            result = 'tie';
             break;
         case playerSelection == 'rock' && computerSelection == 'scissors':
         case playerSelection == 'scissors' && computerSelection == 'paper':
-        case playerSelection == 'paper' && computerSelection == 'rock':           
-            console.log(`Player wins! ${capitaliseFirstLetter(playerSelection)} beats ${computerSelection}`);
-            result = 'player'
+        case playerSelection == 'paper' && computerSelection == 'rock':   
+            roundResultsText.textContent = `Player wins! ${capitaliseFirstLetter(playerSelection)} beats ${computerSelection}`;
+            result = 'player';
             break;
         default:
-            console.log(`Computer wins! ${capitaliseFirstLetter(computerSelection)} beats ${playerSelection}`);
-            result = 'computer'
+            roundResultsText.textContent = `Computer wins! ${capitaliseFirstLetter(computerSelection)} beats ${playerSelection}`;
+            result = 'computer';
             break;
     }
     return result;
@@ -80,31 +76,25 @@ function replayGame() {
 }
 
 function game() { // increments score based on the winner of the round. If there is a tie, it reruns the round
-    let numOfRounds = getNumberOfRounds()
+    
 
-    let playerCount = 0;
-    let computerCount = 0;
+    // for (let i = 0; i < numOfRounds; i++) {
+    //     console.log('Round ' + (i + 1));
+    //     while (true) {
+            
 
-    for (let i = 0; i < numOfRounds; i++) {
-        console.log('Round ' + (i + 1));
-        while (true) {
-            let playerSelection = getPlayerChoice();
-            let computerSelection = getComputerChoice();
-            let result = playRound(computerSelection, playerSelection);
 
-            if (result === 'player') {
-                playerCount += 1;
-                break;
-            } else if (result === 'computer') {
-                computerCount += 1;
-                break;
-            }
-        }
-    }
-    declareOverallResults(playerCount, computerCount)
+    //         if (result === 'player') {
+    //             playerCount += 1;
+    //             break;
+    //         } else if (result === 'computer') {
+    //             computerCount += 1;
+    //             break;
+    //         }
+    //     }
+    // }
+    // declareOverallResults(playerCount, computerCount)
 
-    replayGame()
+    // replayGame()
 }
    
-
-game();
